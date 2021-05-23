a7fc3a550f4728c87c4acf2502026e05715982a3
JBGA21 <jesusbeltran_93@hotmail.com>
2021-05-23 11:14:29 -0500

    function get data done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,16 +1,16 @@
 // create function to get the data
-function getData(sample) {
+function getData(id) {
+    // Reade JSON
     d3.json("data/samples.json").then((data) => {
-        console.log(data)
         let metadata = data.metadata;
         let resultsarray = metadata.filter(sampleobject =>
             sampleobject.id == sample);
         let result = resultsarray[0]
         let panel = d3.select("#sample-metadata");
         panel.html("");
-
-
-
-
-    })
+        Object.entries(result).forEach(([key, value]) => {
+            panel.append("h6").text(`${key}: ${value}`);
+        });
+    });
 }
+
