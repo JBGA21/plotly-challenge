39a191eff49b5588b364b7b3a3d6798a642c76b4
JBGA21 <jesusbeltran_93@hotmail.com>
2021-05-23 12:05:22 -0500

    final changes done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,84 +1,119 @@
-// create function to get the data
-function getData(sample) {
-    // Reade JSON
-    d3.json("data/samples.json").then((data) => {
-        let metadata = data.metadata;
-        let resultsarray = metadata.filter(sampleobject =>
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultsarray = metadata.filter(sampleobject =>
             sampleobject.id == sample);
-        let result = resultsarray[0]
-        let panel = d3.select("#sample-metadata");
+        var result = resultsarray[0]
+        var panel = d3.select("#sample-metadata");
         panel.html("");
         Object.entries(result).forEach(([key, value]) => {
             panel.append("h6").text(`${key}: ${value}`);
         });
+
+        //buildGauge(result.wfreq)
+
+
+
     });
 }
 
-function buildCharts(sample) {
-    d3.json("data/samples.json").then((data) => {
-        let samples = data.samples;
-        let resultsarray = samples.filter(sampleobject =>
-            sampleobject.id == sample);
-        let result = resultsarray[0]
-        let ids = result.otu_ids;
-        let labels = result.otu_labels;
-        let values = result.sample_values;
+//function buildGauge(wfreq) {}
 
-        // Create chart
-        let layoutB = {
+function buildCharts(sample) {
+
+    // Use `d3.json` to fetch the sample data for the plots
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+        var resultsarray = samples.filter(sampleobject =>
+            sampleobject.id == sample);
+        var result = resultsarray[0]
+
+        var ids = result.otu_ids;
+        var labels = result.otu_labels;
+        var values = result.sample_values;
+
+        //------------------------------------------------------//
+        //------------------------------------------------------//
+        // Build a BUBBLE Chart 
+        //------------------------------------------------------//
+        //------------------------------------------------------//
+
+        var LayoutBubble = {
             margin: { t: 0 },
             xaxis: { title: "OTU ID" },
-            hovermode: "closest"
+            hovermode: "closest",
         };
 
-        let dataB = [{
-            x: ids,
-            y: values,
-            text: labels,
-            mode: "markers",
-            marker: {
-                color: ids,
-                size: values
+        var DataBubble = [
+            {
+                x: ids,
+                y: values,
+                text: labels,
+                mode: "markers",
+                marker: {
+                    color: ids,
+                    size: values,
+                }
             }
-        }];
-        Plotly.newPlot("bubble", dataB, layoutB);
+        ];
 
-        let dataBar = [
+        Plotly.newPlot("bubble", DataBubble, LayoutBubble);
+
+
+        //---------------------------------------------------------//
+        //---------------------------------------------------------//
+        //  Build a BAR Chart
+        //---------------------------------------------------------//  
+        //---------------------------------------------------------// 
+        var bar_data = [
             {
                 y: ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
                 x: values.slice(0, 10).reverse(),
                 text: labels.slice(0, 10).reverse(),
                 type: "bar",
                 orientation: "h"
+
             }
         ];
-        let barLayout = {
+
+        var barLayout = {
             title: "Top 10 Bacteria Cultures Found",
             margin: { t: 30, l: 150 }
         };
 
-        Plotly.newPlot("bar", dataBar, barLayout);
+        Plotly.newPlot("bar", bar_data, barLayout);
     });
 }
 
-function init() {
-    let select = d3.select("#selDataset");
 
-    d3.json("data/samples.json").then((data) => {
-        let sampleNames = data.names;
+function init() {
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+
+    // Use the list of sample names to populate the select options
+    d3.json("samples.json").then((data) => {
+        var sampleNames = data.names;
         sampleNames.forEach((sample) => {
-            select.append("option").text(sample).property("value", sample);
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
         });
+
+        // Use the first sample from the list to build the initial plots
         const firstSample = sampleNames[0];
         buildCharts(firstSample);
         buildMetadata(firstSample);
     });
 }
 
-function changed(newSample) {
+function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
     buildCharts(newSample);
     buildMetadata(newSample);
 }
 
-// Init the dashboard
+
+
+// Initialize the dashboard
 init();
